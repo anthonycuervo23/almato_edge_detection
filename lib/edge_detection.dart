8de3c84aef8dda8eae05eb6e2fd43bf0ca27a76a
Jean Cuervo <martoranito@gmail.com>
2023-01-02 16:02:37 +0100

    first commit

---
+++ lib/edge_detection.dart
@@ -0,0 +1,33 @@
+import 'dart:async';
+
+import 'package:flutter/services.dart';
+
+class EdgeDetection {
+  static const MethodChannel _channel = const MethodChannel('edge_detection');
+
+  /// Call this method to scan the object edge in live camera.
+  static Future<String?> detectEdge(
+      {String androidScanTitle: "Scanning",
+      String androidCropTitle: "Crop",
+      String androidCropBlackWhiteTitle: "Black White",
+      String androidCropReset: "Reset"}) async {
+    return await _channel.invokeMethod('edge_detect', {
+      'scan_title': androidScanTitle,
+      'crop_title': androidCropTitle,
+      'crop_black_white_title': androidCropBlackWhiteTitle,
+      'crop_reset_title': androidCropReset,
+    });
+  }
+
+  /// Call this method to scan the object edge from a gallery image.
+  static Future<String?> detectEdgeFromGallery(
+      {String androidCropTitle: "Crop",
+      String androidCropBlackWhiteTitle: "Black White",
+      String androidCropReset: "Reset"}) async {
+    return await _channel.invokeMethod('edge_detect_gallery', {
+      'crop_title': androidCropTitle,
+      'crop_black_white_title': androidCropBlackWhiteTitle,
+      'crop_reset_title': androidCropReset,
+    });
+  }
+}
